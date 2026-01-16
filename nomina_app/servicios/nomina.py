eee1f97a7e591779b95bddc04053ab048819c35f
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-01-16 17:14:47 -0500

    Cree la clase Nomina con constructor, agregar_empleado y listar_empleados

---
+++ nomina_app/servicios/nomina.py
@@ -0,0 +1,26 @@
+# servicios/nomina.py
+# Lógica del sistema (servicios): # Aquí se administra la lista de empleados y se calcula la nómina total.
+
+class Nomina: 
+    """
+    Clase Nomina:
+    Administra una colección (lista) de empleados y permite:
+    - Registrar empleados.
+    - Listarlos.
+    - Calcular y mostrar el pago total.
+
+    Aquí se evidencia POLIMORFISMO cuando se llama calcular_pago()
+    para distintos tipos de empleados (TiempoCompleto / PorHoras).
+    """
+
+    def __init__(self):     # Constructor: crea la lista donde se almacenarán los empleados
+        self.empleados = []   
+
+    def agregar_empleado(self, empleado) -> None:  # Método para agregar un empleado a la lista
+        self.empleados.append(empleado)
+
+    def listar_empleados(self) -> None:     # Método para listar todos los empleados registrados
+        for e in self.empleados:            # Itera sobre la lista e imprime la información de cada empleado
+            print(e.mostrar_informacion())  # Llama al método mostrar_informacion() de cada empleado
+
+
